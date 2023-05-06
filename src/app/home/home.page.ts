e65535eb8c38867dc0b2f977a1ac95b9ea894295
SESA625165 <kishore.setlurradhakrishna@non.se.com>
2023-05-06 22:57:54 +0530

    Initial draft

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +8,24 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
   constructor() {}
+
+  addresses = [{
+    name: 'A. Reddy',
+    adresss: 's/o B. reddy, abc, sas Taulk, Tumkur District - 572137',
+    ph: 9880066835
+  },
+  {
+    name: 'A. Reddy',
+    adresss: 's/o B. reddy, abc, sas Taulk, Tumkur District - 572137',
+    ph: 9880066835
+  },
+  {
+    name: 'A. Reddy',
+    adresss: 's/o B. reddy, abc, sas Taulk, Tumkur District - 572137',
+    ph: 9880066835
+  }]
 }
